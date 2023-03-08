26a0cb469940e3d6db86c4b1b8424422a3ee44e8
Muhammad Usama <the.mgi@outlook.com>
2023-03-08 14:44:20 +0500

    create contact or member

--- src/main/java/com/the_mgi/wildapricot_wrapper/base/util/UtilFunctions.java
+++ src/main/java/com/the_mgi/wildapricot_wrapper/base/util/UtilFunctions.java
@@ -1,5 +1,8 @@
 package com.the_mgi.wildapricot_wrapper.base.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 public class UtilFunctions {
     public static String convertBooleanToString(Boolean b) {
         return b == null ? null : b.toString();
@@ -7,5 +10,11 @@ public class UtilFunctions {
 
     public static String convertIntegerToString(Integer integer) {
         return integer == null ? null : integer.toString();
+    }
+
+    public static String toJsonString(Object object) throws JsonProcessingException {
+        if (object == null) return "";
+        ObjectMapper mapper = ObjectMapperSingleton.getObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
